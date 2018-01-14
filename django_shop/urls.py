7f8ae6fab4992188e976ae7274110cc79c95efb0
Raisa <raisa.abzelimova@yandex.ru>
2018-01-14 18:09:55 +0300

    second

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -38,3 +38,7 @@ if settings.DEBUG == True:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
+    urlpatterns +=static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
